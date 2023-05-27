f1e96c3f07cf697f7c61a89fd73810ecb7fb1245
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2023-05-27 22:49:02 +0400

    basic

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function handleApplyNowClick(event) {
 // Event listener for the "Apply Now" button
 var applyNowButton = document.querySelector(".btn");
 applyNowButton.addEventListener("click", handleApplyNowClick);
-  
+ 
+//for the background image
 window.addEventListener('scroll', function() {
   var scrollPosition = window.pageYOffset;
   document.querySelector('.hero-section').style.backgroundSize = 100 + scrollPosition / 9 + '%';
@@ -23,3 +24,40 @@ window.addEventListener('scroll', function() {
   document.querySelector('.hero-content h1').style.fontSize = 475 + scrollPosition / 5 + '%';
   document.querySelector('btn').style.fontSize = 100 + scrollPosition / 5 + '%';
 });
+
+
+//the about-section
+function isInViewport(element, offsetPercentage) {
+  var rect = element.getBoundingClientRect();
+  var threshold = (window.innerHeight || document.documentElement.clientHeight) * (offsetPercentage / 100);
+  return (
+    rect.top >= -threshold &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + threshold &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function animateElementsOnScroll() {
+  var aboutSections = document.getElementsByClassName("about-section");
+  var programsSections = document.getElementsByClassName("programs-section");
+  var aboutImage = document.getElementsByClassName("about-image")[0];
+
+  for (var i = 0; i < aboutSections.length; i++) {
+    var aboutSection = aboutSections[i];
+    if (isInViewport(aboutSection, 30)) {
+      aboutSection.classList.add("animate");
+      aboutImage.classList.add("animate");
+    }
+  }
+
+  for (var j = 0; j < programsSections.length; j++) {
+    var programsSection = programsSections[j];
+    if (isInViewport(programsSection, 30)) {
+      programsSection.classList.add("animate");
+    }
+  }
+}
+
+window.addEventListener("scroll", animateElementsOnScroll);
+animateElementsOnScroll();
