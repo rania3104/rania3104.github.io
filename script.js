053f663f9547a34680caee80412ed4c8b67fcd40
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2023-05-27 12:26:09 +0400

    basic

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 
 // Function to handle click events on the "Apply Now" button
 function handleApplyNowClick(event) {
-    event.preventDefault();
-    // Add your logic here for handling the click event
-    // For example, you can redirect the user to the application page
-    window.location.href = "application.html";
-  }
+  event.preventDefault();
+  // Add your logic here for handling the click event
+  // For example, you can redirect the user to the application page
+  window.location.href = "application.html";
+}
+
+// Event listener for the "Apply Now" button
+var applyNowButton = document.querySelector(".btn");
+applyNowButton.addEventListener("click", handleApplyNowClick);
   
-  // Event listener for the "Apply Now" button
-  var applyNowButton = document.querySelector(".btn");
-  applyNowButton.addEventListener("click", handleApplyNowClick);
+// Update the background image size on scroll
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.pageYOffset;
+  document.body.style.backgroundSize = 100 + scrollPosition / 5 + '%';
+});
