040b7b61f9a24e33a489c10216dd83ced867bb96
rania3104 <raniaazizfarooqi.14574.11g@gmail.com>
2023-05-30 11:44:23 +0400

    DONE

--- script.js
+++ script.js
@@ -90,4 +90,20 @@ window.addEventListener('scroll', function() {
   }
 });
 
+// Back to Top Button Functionality
+window.addEventListener('scroll', function() {
+  var backToTopButton = document.querySelector('.back-to-top');
+  if (window.scrollY > 300) {
+      backToTopButton.classList.add('show');
+  } else {
+      backToTopButton.classList.remove('show');
+  }
+});
 
+document.querySelector('.back-to-top').addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  });
+});
